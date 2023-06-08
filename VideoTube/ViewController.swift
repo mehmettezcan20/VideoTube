025ff9e2407b139bb69bdd6c36038aab6bae1de2
mehmettezcan20 <108008652+mehmettezcan20@users.noreply.github.com>
2023-06-08 17:47:41 +0300

    renk kımızı

--- VideoTube/ViewController.swift
+++ VideoTube/ViewController.swift
@@ -83,7 +83,7 @@ class ViewController: UIViewController {
                                          target: self,
                                          action: #selector(leftHandAction))
         
-        nav.navigationBar.backgroundColor =  .yellow
+        nav.navigationBar.backgroundColor =  .red
         
         nav.navigationBar.topItem?.rightBarButtonItem = button
         nav.navigationBar.topItem?.title = "VideoTube"
